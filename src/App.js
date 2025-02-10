d8cc60dfa0de576f60b7b16ee2dbd063807e407b
Sathwik <enapothulasathwik09@gmail.com>
2025-02-10 14:51:47 +0530

    cra&tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-center text-3xl text-purple-400">
+      Hello Everyone
+    </div>
+  );
+}
+
+export default App;
