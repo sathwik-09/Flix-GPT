cde8b2ceddabeed5341f01f88f29e2aa19f90bda
Sathwik <enapothulasathwik09@gmail.com>
2025-02-15 23:32:54 +0530

    add constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO_URL = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const profile_avatar ="https://occ-0-3752-3646.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTUxMDQ1NGMwMzA1NWVmMzk3YWE1YTI5NjhkNTc4MiIsIm5iZiI6MTczOTYzMDk0NC4xNDcsInN1YiI6IjY3YjBhOTYwYmExNDcxMmY3NzM2MjZkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IXGfu_syZTPwQ3JfcxF4H6Iu6Qy6RpsvLjsU8BKUqlg'
+    },
+};
