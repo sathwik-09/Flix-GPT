a2ac7fe761c6251dcf1cb35cb856fc9a3ed58de3
Sathwik <enapothulasathwik09@gmail.com>
2025-02-16 16:19:12 +0530

    feat: enhanced player

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,16 +1,15 @@
-import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='pt-36 px-12'>
-      <h1 className='text-6xl font-bold'>{title}</h1>
-      <p className='py-6 text-lg w-1/4'>{overview}</p>
+    <div className='w-screen aspect-video pt-[16%] px-20 absolute text-white bg-gradient-to-r from-black'>
+      <h1 className='text-5xl font-bold'>{title}</h1>
+      <p className='py-6 text-md w-1/4'>{overview}</p>
       <div className=''>
-        <button className='bg-gray-500 text-black p-4 px-10 text-xl bg-opacity-50 rounded-lg'> ▶ Play</button>
-        <button className='mx-2 bg-gray-500 text-black p-4 px-10 text-xl bg-opacity-50 rounded-lg'>More Info</button>
+        <button className='bg-white text-black p-3 px-8 text-xl rounded-lg hover:bg-opacity-70'> ▷ Play</button>
+        <button className='mx-2 bg-gray-500 text-white p-3 px-8 text-xl rounded-lg bg-opacity-50 hover:bg-opacity-70'>More Info</button>
       </div>
     </div>
   )
 }
 
-export default VideoTitle
+export default VideoTitle;
