ba4bcb84a1464188424cfd9941f54c746448f19f
Sathwik <enapothulasathwik09@gmail.com>
2025-02-17 18:59:21 +0530

    feat: add multiple languages

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,16 @@
+const lang = {
+  en: {
+    search : "Search",
+    gptSearchPlaceHolder : "What would you like to watch today?",
+  },
+  telugu: {
+    search : "శోధించండి",
+    gptSearchPlaceHolder: "మీరు ఈరోజు ఏమి చూడాలనుకుంటున్నారు?",
+  },
+  hindi: {
+    search: "खोज",
+    gptSearchPlaceHolder: "आज आप क्या देखना चाहेंगे?"
+  }
+}
+
+export default lang;
