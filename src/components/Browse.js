defc0bda266a1fc89b3b4cd3c15736a1ff9f2bd4
Sathwik <enapothulasathwik09@gmail.com>
2025-02-22 22:42:04 +0530

    feat: add gpt integration for movie recommendation

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,17 +2,26 @@ import Header from "./Header"
 import useNowPlayingMovies from "../Hooks/useNowPlayingMovies";
 import FirstContainer from "./FirstContainer";
 import SecondContainer from "./SecondContainer";
-
+import usePopularMovies from "../Hooks/usePopularMovies";
+import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
 
+  const showGptSearch = useSelector((store)=>store.gpt.showGptSearch);
+
   useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div>
       <Header/>
-      <FirstContainer/>
-      <SecondContainer/>
+      {
+        showGptSearch ? <GptSearch/> : <>
+        <FirstContainer/>
+        <SecondContainer/>
+        </>
+      }
     </div>
   )
 }
