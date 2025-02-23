608afceb4d4515cd43df3560c8d95b90c13b542e
Sathwik <enapothulasathwik09@gmail.com>
2025-02-23 22:41:15 +0530

    updated url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,3 +9,13 @@ export const API_OPTIONS = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTUxMDQ1NGMwMzA1NWVmMzk3YWE1YTI5NjhkNTc4MiIsIm5iZiI6MTczOTYzMDk0NC4xNDcsInN1YiI6IjY3YjBhOTYwYmExNDcxMmY3NzM2MjZkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IXGfu_syZTPwQ3JfcxF4H6Iu6Qy6RpsvLjsU8BKUqlg'
     },
 };
+
+export const IMG_CDN = "https://image.tmdb.org/t/p/w500/"
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/638e9299-0637-42d1-ba39-54ade4cf2bf6/web/IN-en-20250203-TRIFECTA-perspective_46eb8857-face-4ea6-b901-dbf22b461369_large.jpg";
+
+export const SUPPORTED_LANGUAGES = [{identifier : "en", name: "English"}, {identifier : "telugu", name: "Telugu"}, {identifier : "hindi", name: "Hindi"},  ]
+
+export const OPEN_AI_KEY = process.env.REACT_APP_API_KEY;
+
+
