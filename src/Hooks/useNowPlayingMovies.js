3b492ba598e8ee64c5926add627cdcab0ef3711d
Sathwik <enapothulasathwik09@gmail.com>
2025-02-23 22:40:07 +0530

    feat: add nowplaying movies to store

--- src/Hooks/useNowPlayingMovies.js
+++ src/Hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/movieSlice";
 import { API_OPTIONS } from "../utils/constants";
 import { useEffect } from "react";
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
 
+  const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
+
   const getNowPlayingMovies = async () => {
     const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
     const data = await response.json();
@@ -14,7 +16,7 @@ const useNowPlayingMovies = () => {
   }
 
   useEffect(() => {
-    getNowPlayingMovies();
+    if(!nowPlayingMovies) getNowPlayingMovies();
   }, [])
 }
 
